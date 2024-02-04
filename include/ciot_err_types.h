8c63d7f3e9ef6b623f3b26afbd2380c1fb3ba392
wesley-santos <53940899+WesleyJoseSantos@users.noreply.github.com>
2024-02-04 09:32:18 -0300

    feat: first commit

---
+++ include/ciot_err_types.h
@@ -0,0 +1,46 @@
+/**
+ * @file ciot_err_types.h
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2024-02-03
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#ifndef __CIOT_ERR_TYPES__H__
+#define __CIOT_ERR_TYPES__H__
+
+#define CIOT_ERR_BASE 0x0
+
+typedef enum
+{
+    CIOT_FAIL = -1,
+    CIOT_OK,
+    CIOT_ERR_NULL_ARG = CIOT_ERR_BASE + 1,
+    CIOT_ERR_INVALID_ID,
+    CIOT_ERR_INVALID_TYPE,
+    CIOT_ERR_OVERFLOW,
+    CIOT_ERR_NOT_IMPLEMENTED,
+    CIOT_ERR_NOT_SUPPORTED,
+    CIOT_ERR_BUSY,
+    CIOT_ERR_INVALID_STATE,
+    CIOT_ERR_SERIALIZATION,
+    CIOT_ERR_DESERIALIZATION,
+    CIOT_ERR_SEND_DATA,
+    CIOT_ERR_RECV_DATA,
+    CIOT_ERR_INVALID_SIZE,
+    CIOT_ERR_CLOSED,
+    CIOT_ERR_NOT_FOUND,
+    CIOT_ERR_VALIDATION_FAILED,
+    CIOT_ERR_CONNECTION,
+    CIOT_ERR_DISCONNECTION,
+    CIOT_ERR_EXCEPTION,
+    CIOT_ERR_TERMINATOR_MISSING,
+    CIOT_ERR_INVALID_ARG,
+    CIOT_ERR_NO_MEMORY,
+    CIOT_ERR_TIMEOUT,
+} ciot_err_t;
+
+#endif  //!__CIOT_ERR_TYPES__H__
